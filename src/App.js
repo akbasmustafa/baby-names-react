10389d1ede35ebe801e0bf929d357ab820e85653
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-05-11 07:50:21 +0100

    Done Level 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React, { useState } from "react";
 import "./App.css";
 import babyNamesData from "./babyNamesData.json";
 
 function App() {
   //id: 0;  name: "Zahra";  sex: "f";
+  const [searchName, setSearchName] = useState("");
 
   function sortNames(a, b) {
     var nameA = a.name.toUpperCase();
@@ -16,18 +18,37 @@ function App() {
     // names must be equal
     return 0;
   }
+
+  function handleSearch(e) {
+    e.preventDefault();
+    setSearchName(e.target.value);
+  }
   return (
     <div className="App">
-      {babyNamesData.sort(sortNames).map((obj) => {
-        return (
-          <span
-            key={obj.id}
-            className={obj.sex === "m" ? "boyStyle" : "girlStyle"}
-          >
-            {obj.name}
-          </span>
-        );
-      })}
+      <input
+        placeholder="Search name"
+        type="text"
+        value={searchName}
+        onChange={handleSearch}
+      />
+      <div className="nameContainer">
+        {babyNamesData
+          .filter((obj) => {
+            const re = new RegExp(searchName, "i");
+            return re.test(obj.name);
+          })
+          ?.sort(sortNames)
+          .map((obj) => {
+            return (
+              <span
+                key={obj.id}
+                className={obj.sex === "m" ? "boyStyle" : "girlStyle"}
+              >
+                {obj.name}
+              </span>
+            );
+          })}
+      </div>
     </div>
   );
 }
